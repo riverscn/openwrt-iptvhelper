c9fe6c52f68b881a79810f96eacd2d88c6f12ed0
Shun Li <riverscn@gmail.com>
2019-11-20 15:22:00 +0800

    init. commit

---
+++ easyiptv.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+add_to_set(){
+     local ip=$1
+     local subnet=$ip/24
+     ping -W1 -c1 $ip &> /dev/null && return
+     if ! ipset -q test iptv $subnet; then
+         ipset add iptv $subnet
+         echo added $subnet to set iptv
+     fi
+}
+  
+logread -e "easyiptv" -f | \
+     while read line; do
+         ip=$(echo "$line" | sed -r 's|.*DST=([0-9.]+).*|\1|')
+         echo requested $ip
+         add_to_set $ip &
+     done

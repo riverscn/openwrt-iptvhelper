5f72f7dc5733ae75e0e25ddcafdba9d997f99527
Shun Li <riverscn@gmail.com>
2019-11-27 23:21:25 +0800

    migrate to procd

--- iptv-helper.sh
+++ iptv-helper.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 add_to_set(){
      local ip=$1
+     local cfg=$2
      local subnet=$ip/24
      ping -W1 -c1 $ip &> /dev/null && return
-     if ! ipset -q test iptv $subnet; then
-         ipset add iptv $subnet
-         echo added $subnet to set iptv
+     if ! ipset -q test iptv-helper_$cfg $subnet; then
+         ipset add iptv-helper_$cfg $subnet
+         echo added $subnet to set iptv-helper_$cfg
      fi
 }
+
+echo $1
   
-logread -e "iptv-helper" -f | \
+logread -e "iptv-helper\.$1" -f | \
      while read line; do
          ip=$(echo "$line" | sed -r 's|.*DST=([0-9.]+).*|\1|')
          echo requested $ip
-         add_to_set $ip &
+         add_to_set $ip $1&
      done
